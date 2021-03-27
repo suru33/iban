0e0e0e46d0fe6cd4b1fb99d2f894a006df63e927
SuRu <33urus@gmail.com>
2021-03-27 03:19:19 +0200

    added mod97

--- src/iban.rs
+++ src/iban.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -9,4 +10,46 @@ pub struct IBANMetaData {
     pub account_check: bool,
     pub branch: bool,
     pub format: String,
+}
+
+fn mod97(head: &str, tail: &str) -> u32 {
+    if tail.len() > 7 {
+        let h = &(format!("{:02}", head.parse::<u32>().unwrap() % 97) + &tail[0..7])[..];
+        let t = &tail[7..];
+        mod97(h, t)
+    } else {
+        let h_val = head.parse::<u32>().unwrap() % 97;
+        let t_val = (h_val.to_string() + &tail[..]).parse::<u32>().unwrap();
+        98 - (t_val % 97)
+    }
+}
+
+impl IBANMetaData {
+    pub fn get(&self) -> String {
+        let mut rng = rand::thread_rng();
+
+        let format = self.format[4..].to_string() + &self.format[0..4];
+        let mut s = String::new();
+        let mut digits: Vec<i32> = Vec::new();
+        for ch in format.chars() {
+            if ch == '#' {
+                let d = &rng.gen_range(0..10);
+                digits.push(*d);
+                s.push_str(&d.to_string());
+            } else if ch.is_alphabetic() {
+                s.push_str(&(ch as u8 - 55).to_string());
+            } else {
+                s.push(ch);
+            }
+        }
+
+        let checksum = format!("{:02}", mod97(&s[0..9], &s[9..]));
+        let mut iban = self.format.replace("00", &checksum);
+        let mut ix = 0;
+        while iban.contains('#') {
+            iban = iban.replacen('#', &digits[ix].to_string()[..], 1);
+            ix += 1;
+        }
+        iban
+    }
 }
